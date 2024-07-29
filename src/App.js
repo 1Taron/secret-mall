949408155852b0277cc89a21cbf2d018eb6b82ab
Jang URam <90294651+Ukdam@users.noreply.github.com>
2024-07-29 20:25:28 +0900

    css 추가
    css 추가

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import './App.css'
+import './App.css';
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 
 function App() {
-  return (
-    <>
-      <Canvas>
-        <OrbitControls autoRotate={true} />
-        <mesh>
-          <ambientLight intensity={1} />
-          <directionalLight position={[-1, 0, 1]} intensity={0.5} />
-          <boxGeometry args={[2, 2, 2]} />
-          <meshStandardMaterial attach="material" color={0xa3b18a} />
-        </mesh>
-      </Canvas>
-    </>
-  );
+    return (
+        <>
+            <Canvas>
+                <OrbitControls autoRotate={true} />
+                <mesh>
+                    <ambientLight intensity={1} />
+                    <directionalLight position={[-1, 0, 1]} intensity={0.5} />
+                    <boxGeometry args={[2, 2, 2]} />
+                    <meshStandardMaterial attach="material" color={0xa3b18a} />
+                </mesh>
+            </Canvas>
+            <a href="" className="logo_font">
+                asd
+            </a>
+        </>
+    );
 }
 
 export default App;
