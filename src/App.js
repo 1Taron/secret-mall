937939d2a5d86e26e6b68de4036e06adc519fa39
1Taron <she11466@naver.com>
2024-07-29 20:55:26 +0900

    라우터 추가 (수정)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import './App.css';
+import './App.css'
 import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import MainPage from './pages/Mainpage';
+import Product from './pages/Product';
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-    return (
-        <>
-            <Canvas>
-                <OrbitControls autoRotate={true} />
-                <mesh>
-                    <ambientLight intensity={1} />
-                    <directionalLight position={[-1, 0, 1]} intensity={0.5} />
-                    <boxGeometry args={[2, 2, 2]} />
-                    <meshStandardMaterial attach="material" color={0xa3b18a} />
-                </mesh>
-            </Canvas>
-            <a href="" className="logo_font">
-                asdasd
-            </a>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/Product" element={<Product />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
